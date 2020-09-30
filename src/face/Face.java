e4fd931ab45fac4ff53afc2f5312f1b933f91212
just6660 <just6660@ISS2TZQKH2.scdsb.pri>
2020-09-30 13:46:21 -0400

    done face

--- src/face/Face.java
+++ src/face/Face.java
@@ -1,7 +1,112 @@
 
 package face;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
 
 public class Face {
+    
+    private int xPos, yPos, eye1xPos, eye2xPos, diameter;
+    private Color facecolor, eyecolor;
+    private Graphics g;
+    private boolean isHappy;
+    
+    public Face(Graphics g, int x, int y){
+        xPos = x;
+        yPos = y;
+        
+        //let the "g" property = the "g" param
+        this.g = g;
+        //defualt size and color
+        diameter = 100;
+        facecolor = Color.red;
+        eyecolor = Color.blue;
+        isHappy = true;
+        
+    }
+    
+    
+    private void drawHead(){
+       g.setColor(facecolor);
+       g.drawOval(xPos,yPos,diameter,diameter);
+       g.fillOval(xPos,yPos,diameter,diameter);
+       
+    }
+    private void drawEyes(){
+        eye1xPos = xPos + diameter/5;
+        eye2xPos = xPos + diameter *2/3;
+        g.setColor(eyecolor);
+        g.drawOval(eye1xPos, yPos + diameter/4, diameter/6, diameter/6);
+        g.drawOval(eye2xPos, yPos + diameter/4, diameter/6,diameter/6);
+        g.fillOval(eye1xPos, yPos + diameter/4, diameter/6, diameter/6);
+        g.fillOval(eye2xPos, yPos + diameter/4, diameter/6,diameter/6);
+    }
+    private void drawMouth(){
+        eye1xPos = xPos + diameter/5;
+        eye2xPos = xPos + diameter *2/3;
+        g.setColor(Color.black);
+        g.drawLine((eye1xPos + eye1xPos + diameter/6) /2, yPos + diameter*3/4, (eye2xPos + eye2xPos + diameter/6) /2, yPos+diameter*3/4);
+        g.drawLine((eye1xPos+eye1xPos+diameter/6)/2,yPos+diameter*3/4,eye1xPos,yPos + diameter*3/5);
+        g.drawLine((eye2xPos+eye2xPos+diameter/6)/2, yPos+diameter*3/4,eye2xPos + diameter/6, yPos + diameter*3/5);
+    }
+    private void drawSadMouth(){
+        eye1xPos = xPos + diameter/5;
+        eye2xPos = xPos + diameter *2/3;
+        g.setColor(Color.black);
+        g.drawLine((eye1xPos + eye1xPos + diameter/6) /2, yPos + diameter*3/5, (eye2xPos + eye2xPos + diameter/6) /2, yPos+diameter*3/5);
+        g.drawLine((eye1xPos+eye1xPos+diameter/6)/2,yPos+diameter*3/5,eye1xPos,yPos + diameter * 3/4);
+        g.drawLine((eye2xPos+eye2xPos+diameter/6)/2, yPos+diameter*3/5,eye2xPos + diameter/6, yPos + diameter*3/4);
+        
+    }
+    
+    public void draw(){
+        drawHead();
+        drawEyes();
+        if(isHappy){
+            drawMouth();
+        }
+        else if(!isHappy){
+            drawSadMouth();
+        }
+       
+            
+        
+    }
+    
+    public void erase(){
+        g.setColor(Color.white);
+        g.fillRect(xPos-10,yPos-10, diameter +20, diameter+20);
+        
+    }
+    
+    public void move(int x, int y){
+        xPos = x;
+        yPos = y;
+        
+    }
+    
+    public void setFaceColor(Color c){
+        facecolor = c;
+        
+    }
+    public void setEyeColor(Color c){
+        eyecolor = c;
+    }
+    
+    public void resizeFace(int d){
+        diameter = d;
+    }
+    
+    public void toggleMood(boolean h){
+        isHappy = h;
+    }
+    
+
+    
+    
+    
+    
     
 }
---
+++ src/face/FaceMaker.java
@@ -0,0 +1,210 @@
+package face;
+
+import java.awt.Color;
+import javax.swing.JColorChooser;
+import javax.swing.JOptionPane;
+
+public class FaceMaker extends javax.swing.JFrame {
+
+    Face f;
+
+    public FaceMaker() {
+        initComponents();
+        f = new Face(jPanel1.getGraphics(), 100, 100);
+        jButton1.setEnabled(false);
+        jButton2.setEnabled(false);
+        jButton3.setEnabled(false);
+        jButton4.setEnabled(false);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jButton5 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0), 7));
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 336, Short.MAX_VALUE)
+        );
+
+        jButton1.setText("Move Face");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jButton2.setText("Resize Face");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        jButton3.setText("Change Colour");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
+        jButton4.setText("Toggle Mood");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+
+        jButton5.setText("Set Initial Face");
+        jButton5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton5ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jButton1)
+                        .addGap(96, 96, 96)
+                        .addComponent(jButton2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 116, Short.MAX_VALUE)
+                        .addComponent(jButton3)
+                        .addGap(90, 90, 90)
+                        .addComponent(jButton4))
+                    .addComponent(jButton5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(7, 7, 7)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton1)
+                    .addComponent(jButton2)
+                    .addComponent(jButton3)
+                    .addComponent(jButton4))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jButton5, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
+        jButton1.setEnabled(true);
+        jButton2.setEnabled(true);
+        jButton3.setEnabled(true);
+        jButton4.setEnabled(true);
+        f.erase();
+        f = new Face(jPanel1.getGraphics(), 50, 50);
+        f.draw();
+
+    }//GEN-LAST:event_jButton5ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        int newx = Integer.parseInt(JOptionPane.showInputDialog(this, "Enter new X"));
+        int newy = Integer.parseInt(JOptionPane.showInputDialog(this, "Enter new Y"));
+        f.erase();
+        f.move(newx, newy);
+        f.draw();
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        Color newColor = JColorChooser.showDialog(this, "Pick New Face Color", Color.red);
+        f.setFaceColor(newColor);
+        Color newColor2 = JColorChooser.showDialog(this, "Pick New Eye Color", Color.red);
+        f.setEyeColor(newColor2);
+        f.erase();
+        f.draw();
+
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        int newd = Integer.parseInt(JOptionPane.showInputDialog(this, "Enter new diameter"));
+        f.erase();
+        f.resizeFace(newd);
+        f.draw();
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+
+        boolean newh = Boolean.parseBoolean(JOptionPane.showInputDialog(this, "Is the face happy? Enter true or false"));
+
+        f.erase();
+        f.toggleMood(newh);
+        f.draw();
+
+    }//GEN-LAST:event_jButton4ActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(FaceMaker.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(FaceMaker.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(FaceMaker.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(FaceMaker.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new FaceMaker().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton5;
+    private javax.swing.JPanel jPanel1;
+    // End of variables declaration//GEN-END:variables
+}
